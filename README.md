21ebd30600bfb4bfe6886d67f600eb9380238929
Dylan Beattie <dylan@dylanbeattie.net>
2023-10-14 00:26:05 +0100

    Testing EF migration via GitHub actions step

--- README.md
+++
@@ -1,2 +0,0 @@
-# rockaway
-Rockaway
--- Rockaway.WebApp/Program.cs
+++ Rockaway.WebApp/Program.cs
@@ -1,21 +1,49 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Rockaway.WebApp.Data;
+using Rockaway.WebApp.Hosting;
 using Rockaway.WebApp.Services;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
 Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
-Log.Information("Serilog support enabled! Yeah!");
+var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog();
 
-// Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IStatusReporter>(new StatusReporter());
+
+Log.Information("Rockaway running in {environment} environment", builder.Environment.EnvironmentName);
+if (builder.Environment.UseSqlite()) {
+	Log.Information("Using Sqlite database");
+	var sqliteConnection = new SqliteConnection("Data Source=:memory:");
+	sqliteConnection.Open();
+	builder.Services.AddDbContext<RockawayDbContext>(options => options.UseSqlite(sqliteConnection));
+} else {
+	Log.Information("Using SQL Server database");
+	var connectionString = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+	builder.Services.AddDbContext<RockawayDbContext>(options => options.UseSqlServer(connectionString));
+}
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
+if (app.Environment.IsProduction()) {
 	app.UseExceptionHandler("/Error");
 	app.UseHsts();
+} else {
+	app.UseDeveloperExceptionPage();
+}
+
+using (var scope = app.Services.CreateScope()) {
+	using var db = scope.ServiceProvider.GetService<RockawayDbContext>()!;
+	if (app.Environment.UseSqlite()) {
+		db.Database.EnsureCreated();
+	} else if (Boolean.TryParse(app.Configuration["apply-migrations"], out var applyMigrations) && applyMigrations) {
+		Log.Information("apply-migrations=true was specified. Applying EF migrations and then exiting.");
+		db.Database.Migrate();
+		Log.Information("EF database migrations applied successfully.");
+		Environment.Exit(0);
+	}
 }
 
 app.UseHttpsRedirection();
@@ -27,4 +55,5 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 app.MapGet("/status", (IStatusReporter reporter) => reporter.GetStatus());
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.Run();
